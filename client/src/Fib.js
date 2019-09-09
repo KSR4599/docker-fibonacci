0d2032ece191bf1673766f7780489d7fe49fe826
KSR <ksreddy9945@gmail.com>
2019-09-09 12:02:45 +0530

    Local Dev Containers

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -2,81 +2,76 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Fib extends Component {
-    state = {
-        seenIndexes: [],
-        values: {},
-        index: ''
-    };
+  state = {
+    seenIndexes: [],
+    values: {},
+    index: ''
+  };
 
-    componentDidMount() {
-        this.fetchValues();
-        this.fetchIndexes();
+  componentDidMount() {
+    this.fetchValues();
+    this.fetchIndexes();
+  }
+
+  async fetchValues() {
+    const values = await axios.get('/api/values/current');
+    this.setState({ values: values.data });
+  }
+
+  async fetchIndexes() {
+    const seenIndexes = await axios.get('/api/values/all');
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    await axios.post('/api/values', {
+      index: this.state.index
+    });
+    this.setState({ index: '' });
+  };
+
+  renderSeenIndexes() {
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+  }
+
+  renderValues() {
+    const entries = [];
+
+    for (let key in this.state.values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {this.state.values[key]}
+        </div>
+      );
     }
 
-    async fetchValues() {
-        const values = await axios.get('/api/values/current');
-        this.setState({ values : values.data});
-    }
+    return entries;
+  }
 
-    async fetchIndexes() {
-        const seenIndexes = await axios.get('/api/values/all');
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index:</label>
+          <input
+            value={this.state.index}
+            onChange={event => this.setState({ index: event.target.value })}
+          />
+          <button>Submit</button>
+        </form>
 
-        this.setState({
-            seenIndexes: seenIndexes.data
-        });
+        <h3>Indexes I have seen:</h3>
+        {this.renderSeenIndexes()}
 
-    }
-
-handleSubmit = async (event) => {
-event.preventDefault();
-
-await axios.post('/api/values', {
-    index: this.state.index
-});
-this.setState({ index: ''});
-};
-
-//Renders Data Coming From Postgres
-    renderSeenIndexes() {
-        return this.state.seenIndexes.map(({ number} ) => {
-            number
-        }).join(', ');
-    }
-//Renders Data Coming from Redis
-    renderValues(){
-        const entries = [];
-
-       for(let key in this.state.values) {
-          entries.push(
-              <div key={key}>
-                For index {key} I calculated {this.state.values[key]}
-               </div>
-          );
-       }
-    }
-
-    render() {
-        return (
-            <div>
-            <form onSubmit={this.handleSubmit}>
-                <label>Enter the index:</label>
-                <input 
-                    value={this.state.index}
-                    onChange={event => this.setState({ index: event.target.value})}
-                />
-                <button>Submit</button>
-            </form>
-
-            <h3>Indexes I have seen:</h3>
-            {this.renderSeenIndexes()}
-
-           <h3>Calculated Values:</h3>
-           {this.renderValues()}
-           </div>
-          
-        );
-        
-    }
+        <h3>Calculated Values:</h3>
+        {this.renderValues()}
+      </div>
+    );
+  }
 }
 
 export default Fib;
--- server/index.js
+++ server/index.js
@@ -1,65 +1,61 @@
 const keys = require('./keys');
 
-
-//Express Setup
-const Express = require('express');
+// Express App Setup
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.join());
+app.use(bodyParser.json());
 
-//Postgres Client Setup
+// Postgres Client Setup
 const { Pool } = require('pg');
 const pgClient = new Pool({
-    user: keys.pgUser,
-    host: keys.pgHost,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort
-
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
 });
+pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient.on('error', () => console.log('Lost Postgres Connection :('));
+pgClient
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+  .catch(err => console.log(err));
 
-pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
- .catch((err) => console.log("Error in Creating the Table!. Error :-", err));
-
- // Redis Client Setup
-
- const redis = require('redis');
+// Redis Client Setup
+const redis = require('redis');
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
-
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 });
-
 const redisPublisher = redisClient.duplicate();
 
-//Express route Handlers
+// Express route handlers
 
-app.get('/', (req,res) => {
-    res.send('Fibonacci');
+app.get('/', (req, res) => {
+  res.send('Hi');
 });
 
-app.get('/values/all', async(req, res) => {
-    const values = await pgClient.query('SELECT * FROM values');
-    res.send(values.rows);
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
 
+  res.send(values.rows);
 });
 
-app.get('/values/current', async(req, res) => {
-    redisClient.hgetall('values', (err, values) => {
-         res.send(values);
-    });
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
 });
 
-app.post('/values', async(req, res) => {
-    const index = req.body.index;
-  if(parseInt(index) > 40) {
-      return res.status(422).send('Index too high!');
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
   }
 
   redisClient.hset('values', index, 'Nothing yet!');
@@ -67,13 +63,8 @@ app.post('/values', async(req, res) => {
   pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
   res.send({ working: true });
-
 });
 
 app.listen(5000, err => {
-    console.log('Express Listening on Port:5000');
-})
-
-
-
-
+  console.log('Listening');
+});
